4617e59fa8ea15a0670f3515d52221dcd4bc0ec7
amineladraa1 <amineladraa67@gmail.com>
2023-08-16 08:38:27 -0700

    3. Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    union = list(filter(lambda x : x in set_2,set_1))
+    union = list(filter(lambda x :x in set_2, set_1))
     return union
