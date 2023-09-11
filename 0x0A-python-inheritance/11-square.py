d1e2c1125996e6f360143196a7a88f81f2451742
amineladraa1 <amineladraa67@gmail.com>
2023-09-11 08:47:31 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represent a square."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__width, self.__height)
