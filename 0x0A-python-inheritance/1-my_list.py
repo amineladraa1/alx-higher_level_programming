ef2ef07da7a338572222543700feaca063196391
amineladraa1 <amineladraa67@gmail.com>
2023-09-11 06:10:06 -0700

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a subclass list with a print sorted mehode."""
+
+
+class MyList(list):
+    """sorts a list"""
+
+    def print_sorted(self):
+        """return a sorted list"""
+        return sorted(self)
