eafc938cf4521f6f565525d68abdf00adddae064
amineladraa1 <amineladraa67@gmail.com>
2023-09-12 11:24:29 -0700

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""define a script that adds all arguments to a Python list, and then save them to a file"""
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-import sys
+"""define script adds all args to Python list, and then save them to a file"""
 
 
 def add_to_list(args):
@@ -19,6 +19,7 @@ def add_to_list(args):
 
     my_list.extend(args)
     save_to_json_file(my_list, filename)
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
