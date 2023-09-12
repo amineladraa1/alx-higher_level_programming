47f151a5e68c23ea9d815a9f832b6f002a071e40
amineladraa1 <amineladraa67@gmail.com>
2023-09-12 11:21:41 -0700

    7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""define a script that adds all arguments to a Python list, and then save them to a file"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
+
+
+def add_to_list(args):
+    """adds arguments to list
+
+    args:
+        arguments: script args
+    """
+    filename = "add_item.json"
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(args)
+    save_to_json_file(my_list, filename)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    add_to_list(args)
