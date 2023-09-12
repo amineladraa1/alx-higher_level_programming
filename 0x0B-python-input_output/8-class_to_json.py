19a8ced3d2dabea5370cea09527b0730f94a3f9e
amineladraa1 <amineladraa67@gmail.com>
2023-09-12 12:14:08 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,10 +8,8 @@ def class_to_json(obj):
     args:
         obj: object
     """
-    if not hasattr(obj, '__dic__'):
-        raise ValueError("Input object is not an instance of a class")
-
     my_dict = {}
+
     for key, value in obj.__dict__.items():
         if isinstance(value, (list, dict, int, bool, str)):
             my_dict[key] = value
