10f19802a077b4940fa562b0ba863feb7a02ed3c
amineladraa1 <amineladraa67@gmail.com>
2023-11-17 02:49:30 -0800

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """states models
 """
-
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
     db_host = "localhost"
-    db_user = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
+    db_user = sys.argv[1]  # "your_username"
+    db_password = sys.argv[2]  # "your_password"
+    db_name = sys.argv[3]  # "your_database_name"
     port = 3306
 
     db = MySQLdb.connect(
